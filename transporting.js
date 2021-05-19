23358b120da08d8e70807ffbc7ddae94146309f0
hunksammy <hindustantoysrs@gmail.com>
2021-05-19 12:24:52 +0530

    T

--- transporting.js
+++ transporting.js
@@ -30,7 +30,8 @@ const displayCharacters = (characters) => {
             return `
             <li class="character">
                 <h2>${character.name}</h2><br> <br> <br> <br>
-                <p>${character.house}</p>
+                <p>${character.house}</p><br> <br> <br> <br>
+				<p>${character.transport1}</p>
                 
             </li>
         `;
